1ec367a4efaf43c8f8aa79bdcf39de4465b10873
13-suraj <surajkeshari260@gmail.com>
2025-09-28 02:00:13 +0530

    Model Evaluation

--- src/chickenDisease/config/configuration.py
+++ src/chickenDisease/config/configuration.py
@@ -1,7 +1,8 @@
 from chickenDisease.constants import *
 from chickenDisease.utils.common import read_yaml, create_directories
-from chickenDisease.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from chickenDisease.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig, EvaluationConfig
 import os
+from pathlib import Path
 
 class ConfigurationManager:
     def __init__(
@@ -75,3 +76,12 @@ class ConfigurationManager:
         )
         return training_config
 
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model = Path("artifacts/training/model.keras"),
+            training_data = Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params = self.params,
+            params_image_size = self.params.IMAGE_SIZE,
+            params_batch_size = self.params.BATCH_SIZE 
+        )
+        return eval_config
